8c9aafb4dd40a4fdcc44f9521324b1025eed00a6
elhadyLemtai <78748781+elhadyLemtai@users.noreply.github.com>
2021-03-13 23:06:13 +0100

    Update elfirstpygit.py
    (( ))  2021

--- elfirstpygit.py
+++ elfirstpygit.py
@@ -7,7 +7,7 @@ def is_pm(n):
            m_p = (1 << p) - 1
            s = 4
            for i in range(3, p + 1):
-                s = ((s ** 2)-2 % m_p
+                s = ((s ** 2)-2) % m_p
             return( s==0 )
 print(is_pm(37))
 print(37)
